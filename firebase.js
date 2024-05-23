ae355db85d119236e7e7a5045c5f9b6d376cc66e
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-23 23:11:19 +0100

    delete unwanted files

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdTTjjf05nJW2h6HpZqgfn4AiWluTN1bg",
+  authDomain: "slack-clone-499bc.firebaseapp.com",
+  projectId: "slack-clone-499bc",
+  storageBucket: "slack-clone-499bc.appspot.com",
+  messagingSenderId: "790677283734",
+  appId: "1:790677283734:web:1fe59adc9a28f1310d33b7",
+};
