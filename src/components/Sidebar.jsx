041c2a20206220ced7abf063d9de28e8406da333
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-25 13:22:33 +0100

    styled: sidebarheader

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { FiberManualRecordOutlined } from "@mui/icons-material";
+import { Create, FiberManualRecord } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
@@ -9,10 +9,11 @@ function Sidebar() {
         <SidebarInfo>
           <h2>BuzzRoom HQ</h2>
           <h3>
-            <FiberManualRecordOutlined />
+            <FiberManualRecord />
             Gethin Davies
           </h3>
         </SidebarInfo>
+        <Create />
       </SidebarHeader>
     </SidebarContainer>
   );
@@ -29,6 +30,40 @@ const SidebarContainer = styled.div`
   margin-top: 60px;
 `;
 
-const SidebarHeader = styled.div``;
+const SidebarHeader = styled.div`
+  display: flex;
+  border-bottom: 1px solid var(--buzz-room-color);
+  padding: 13px;
 
-const SidebarInfo = styled.div``;
+  > .MuiSvgIcon-root {
+    padding: 8px;
+    color: var(--buzz-room-color);
+    font-size: 18px;
+    background-color: white;
+    border-radius: 999px;
+  }
+`;
+
+const SidebarInfo = styled.div`
+  flex: 1;
+
+  > h2 {
+    font-size: 15px;
+    font-weight: 900;
+    margin-bottom: 5px;
+  }
+
+  > h3 {
+    display: flex;
+    font-size: 13px;
+    font-weight: 400;
+    align-items: center;
+  }
+
+  > h3 > .MuiSvgIcon-root {
+    font-size: 14px;
+    margin-top: 1px;
+    margin-right: 2px;
+    color: green;
+  }
+`;
