9abb1bdab86d618a81824150ed5da214176a241e
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-26 15:28:42 +0100

    styled: chat-header

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,23 @@
+import { InfoOutlined, StarBorderOutlined } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
 function Chat() {
   return (
     <ChatContainer>
-      <h1>Hello</h1>
+      <Header>
+        <HeaderLeft>
+          <h4>
+            <strong>#Room-name</strong>
+            <StarBorderOutlined />
+          </h4>
+        </HeaderLeft>
+        <HeaderRight>
+          <p>
+            <InfoOutlined /> Details
+          </p>
+        </HeaderRight>
+      </Header>
     </ChatContainer>
   );
 }
@@ -16,4 +29,48 @@ const ChatContainer = styled.div`
   flex-grow: 1;
   overflow-y: scroll;
   margin-top: 60px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid #49274b;
+  border-top: 1px solid #49274b;
+  border-left: 1px solid #49274b;
+  background-color: black;
+  height: 24px;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+
+  > h4 {
+    display: flex;
+    text-transform: lowercase;
+    margin-right: 10px;
+    color: whitesmoke;
+  }
+
+  > h4 > .MuiSvgIcon-root {
+    margin-left: 10px;
+    font-size: 18px;
+    color: whitesmoke;
+  }
+`;
+
+const HeaderRight = styled.div`
+  > p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    color: whitesmoke;
+  }
+
+  > p > .MuiSvgIcon-root {
+    margin-right: 5px !important;
+    font-size: 16px;
+    color: whitesmoke;
+  }
 `;
