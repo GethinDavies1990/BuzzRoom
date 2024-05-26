fec8726fd0e28228598b5b6bf7c95d28e03ba17d
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-26 21:50:39 +0100

    styled: login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,5 +16,23 @@ function Login() {
 
 export default Login;
 
-const LoginContainer = styled.div``;
-const LoginInnerContainer = styled.div``;
+const LoginContainer = styled.div`
+  background-color: aliceblue;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+
+const LoginInnerContainer = styled.div`
+  padding: 100px;
+  text-align: center;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 024);
+
+  > img {
+    object-fit: contain;
+    height: 100px;
+    margin-bottom: 40px;
+  }
+`;
